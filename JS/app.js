73096302e38fe154b9c9cb9b0e6cb435593ab68a
Yohane ENDOKE <56687011+Yohane07@users.noreply.github.com>
2022-06-16 23:49:12 +0200

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,25 @@
+
+// OBJET WINDOWS/ a revoir à partir du windows.setInterval();
+/*
+function aleatoire(maxalea) {
+    return Math.floor(Math.random()*maxalea);
+}
+
+var nombreAleatoire = aleatoire(2);
+
+var reponse = window.prompt("Devinez mon nombre");
+
+while (reponse != nombreAleatoire) {
+    if (reponse < nombreAleatoire) {
+        alert("Ressaies c'est trop bas, le nombre était " + nombreAleatoire )
+    } else {
+        alert("Ressaies c'est trop haut, le nombre était " + nombreAleatoire)
+    }
+    reponse = window.prompt("Devinez mon nombre");
+}
+alert("Bien joué champion " + nombreAleatoire);
+
+ */
+
+// LE DOM:
+
