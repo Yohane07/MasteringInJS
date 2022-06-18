4fd6598068336941a1b267f61745329ce0a7116c
pageAccueil <yohaneendoke4@gmail.com>
2022-06-18 03:39:57 +0200

    modif utilisation du DOM

---
+++ app.js
@@ -0,0 +1,60 @@
+
+// OBJET WINDOWS/ a revoir à partir du windows.setInterval();
+/*
+function aleatoire(maxalea) {
+    return Math.floor(Math.random()*maxalea);
+}
+
+var nombreAleatoire = aleatoire(2);
+
+var reponse = window.prompt("Devinez mon nombre");
+
+while (reponse != nombreAleatoire) {
+    if (reponse < nombreAleatoire) {
+        alert("Ressaies c'est trop bas, le nombre était " + nombreAleatoire )
+    } else {
+        alert("Ressaies c'est trop haut, le nombre était " + nombreAleatoire)
+    }
+    reponse = window.prompt("Devinez mon nombre");
+}
+alert("Bien joué champion " + nombreAleatoire);
+
+ */
+
+// LE DOM:
+
+// var p = document.querySelector('.paragraph');
+
+
+/* L'IDEE était d'avoir une phrase qui change toute les secondes ou les dis secondes un peu comme une conversation
+
+var element = ['hey Creative', 'You good ?', 'Welcome'];
+
+function boucle (){
+        for (var i; i<element.length; i++){
+           console.log(element);
+        }
+    window.setInterval(function (){
+        p.classList.toggle('element')
+    }, 1000)
+}
+
+boucle();
+
+*/
+
+// window.setInterval(function (){
+//     p.classList.toggle('orange')
+// }, 1000)
+
+var para = document.querySelectorAll('p');
+
+for (var i = 0; i < para.length; i++){
+    (function(p){
+        window.setInterval(function (){
+            p.classList.toggle('orange')
+        }, 1000)
+    })(para[i]);
+}
+
+//
